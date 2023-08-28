c6ae2498671d5036247af0a8ff423e5798eedfa3
Luke Thorne <the.thorne48@gmail.com>
2023-08-28 14:32:44 -0500

    make port configurable

--- wol.go
+++ wol.go
@@ -3,12 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 var port int
 
 func main() {
-	port = 80
+	p := os.Getenv("PORT")
+	if p != "" {
+		var err error
+		port, err = strconv.Atoi(p)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		port = 8080
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		var mac = r.URL.Query().Get("mac")
 		fmt.Fprintf(w, "Broadcasting WOL request")
